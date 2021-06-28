b4e92f8406e039caf4b201ae691ab1ad047fe15f
blacktechiegirl <84915808+blacktechiegirl@users.noreply.github.com>
2021-06-28 15:30:43 +0100

    Add files via upload

---
+++ src/components/emoji.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Emoji(props){
+  return (
+    
+        <div className="term">
+          <dt>
+            <span className="emoji" role="img" aria-label="Tense Biceps">
+              {props.icon}
+            </span>
+            <span>{props.label}</span>
+          </dt>
+          <dd>
+            {props.meaning}
+          </dd>
+        </div>
+    
+  )};
+
+export default Emoji;
---
+++ src/emojipedia.js
@@ -0,0 +1,49 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "💪",
+    name: "Tense Biceps",
+    meaning:
+      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+  },
+  {
+    id: 2,
+    emoji: "🙏",
+    name: "Person With Folded Hands",
+    meaning:
+      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+  },
+  {
+    id: 3,
+    emoji: "🤣",
+    name: "Rolling On The Floor, Laughing",
+    meaning:
+      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+  },
+
+  {
+    id: 4,
+    emoji: "😄",
+    name: "Grinning Face with Smiling Eyes",
+    meaning:
+      "A yellow face with smiling eyes and a broad, open smile, showing upper teeth and tongue on some platforms. Often conveys general happiness and good-natured amusement."
+  },
+
+  {
+    id: 5,
+    emoji: "😍",
+    name: "Smiling Face with Heart-Eyes",
+    meaning:
+      "A yellow face with an open smile, sometimes showing teeth, and red, cartoon-styled hearts for eyes. Often conveys enthusiastic feelings of love, infatuation, and adoration, e.g., I love/am in love with this person or thing."
+  },
+  
+  {
+    id: 6,
+    emoji: "😜",
+    name: "Winking Face with Tongue",
+    meaning:
+      "A yellow face sticking out its tongue and making a zany wink, usually shown with its right eye closed and left wide open. Often conveys a sense of fun, excitement, wackiness, buffoonery, or joking."
+  }
+];
+
+export default emojipedia;
